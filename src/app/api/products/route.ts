f3d6808948b1a3c83e36ed37e859319fd2be160b
Cat McGee <hello@mcgee.cat>
2025-08-23 18:22:48 -0600

    fix build

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,9 +1,7 @@
 import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-06-20",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export async function GET() {
   try {
